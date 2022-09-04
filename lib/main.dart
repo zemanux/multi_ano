b4a46580e2b795e58184eac82f29bc2ed594c24d
Emmanuel Lefebvre <Emmanuel_LEFEBVRE@connect-tech.sncf>
2022-09-04 16:51:33 +0200

    example

--- lib/main.dart
+++ lib/main.dart
@@ -5,94 +5,111 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   static const String _title = 'Flutter Code Sample';
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final controller = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      const scrollOffset = 250.0;
+
+      controller.jumpTo(250);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: _title,
-      home: NestedScrollViewTestWidget(),
-    );
+    return NestedScrollViewTestWidget(controller: controller);
   }
 }
 
 class NestedScrollViewTestWidget extends StatelessWidget {
-  const NestedScrollViewTestWidget({Key? key}) : super(key: key);
+  const NestedScrollViewTestWidget({Key? key, required this.controller})
+      : super(key: key);
+
+  final ScrollController controller;
+
+  static const itemExtent = 48.0;
 
   @override
   Widget build(BuildContext context) {
-    final List<String> tabs = <String>['Tab 1', 'Tab 2'];
-    return DefaultTabController(
-      length: tabs.length, // This is the number of tabs.
-      child: Scaffold(
-        body: NestedScrollView(
-          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-            return <Widget>[
-              SliverOverlapAbsorber(
-                handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                sliver: MediaQuery.removePadding(
-                  context: context,
-                  removeBottom: true,
-                  child: MultiSliver(
-                    children: [
-                      const SliverPinnedHeader(
-                          child: SafeArea(child: Padding(padding: EdgeInsets.all(16), child: Text('Test')))),
-                      SliverAppBar(
-                        title: const Text('Books'), // This is the title in the app bar.
-                        pinned: true,
-                        expandedHeight: 150.0,
-                        forceElevated: innerBoxIsScrolled,
-                        bottom: TabBar(
-                          tabs: tabs.map((String name) => Tab(text: name)).toList(),
-                        ),
+    const tabs = <String>['Tab 1', 'Tab 2'];
+
+    final nestedScrollView = NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverOverlapAbsorber(
+            handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+            sliver: MultiSliver(
+              nestedScrollView: true,
+              children: [
+                const SliverPinnedHeader(
+                    child: Padding(
+                        padding: EdgeInsets.all(16), child: Text('Bar 1'))),
+                const SliverToBoxAdapter(
+                    child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                        child: Text('Text 1'))),
+                SliverPinnedHeader(
+                    child: Material(
+                        color: Colors.green,
+                        child: TabBar(
+                            tabs: tabs.map((t) => Tab(text: t)).toList()))),
+              ],
+            ),
+          ),
+        ];
+      },
+      body: TabBarView(
+        children: tabs.map((String name) {
+          return Material(
+            color: Colors.red,
+            child: Builder(
+              builder: (BuildContext context) {
+                return RefreshIndicator(
+                  edgeOffset:
+                      NestedScrollView.sliverOverlapAbsorberHandleFor(context)
+                              .layoutExtent ??
+                          0,
+                  onRefresh: () async =>
+                      Future.delayed(const Duration(seconds: 1)),
+                  child: CustomScrollView(
+                    controller: controller,
+                    key: PageStorageKey<String>(name),
+                    slivers: <Widget>[
+                      SliverOverlapInjector(
+                        handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                            context),
                       ),
+                      SliverFillRemaining(
+                        // hasScrollBody: false,
+                        child: Center(child: Text(name)),
+                      )
                     ],
                   ),
-                ),
-              ),
-            ];
-          },
-          body: TabBarView(
-            // These are the contents of the tab views, below the tabs.
-            children: tabs.map((String name) {
-              return SafeArea(
-                top: false,
-                bottom: false,
-                child: Builder(
-                  builder: (BuildContext context) {
-                    return CustomScrollView(
-                      key: PageStorageKey<String>(name),
-                      slivers: <Widget>[
-                        SliverOverlapInjector(
-                          // This is the flip side of the SliverOverlapAbsorber
-                          // above.
-                          handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-                        ),
-                        SliverPadding(
-                          padding: const EdgeInsets.all(8.0),
-                          sliver: SliverFixedExtentList(
-                            itemExtent: 48.0,
-                            delegate: SliverChildBuilderDelegate(
-                              (BuildContext context, int index) {
-                                return ListTile(
-                                  title: Text('Item $index'),
-                                );
-                              },
-                              childCount: 30,
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              );
-            }).toList(),
-          ),
-        ),
+                );
+              },
+            ),
+          );
+        }).toList(),
+      ),
+    );
+
+    return MaterialApp(
+      home: DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(body: nestedScrollView),
       ),
     );
   }
